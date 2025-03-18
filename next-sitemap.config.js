acea2e21c088be1d27d86ce810051dafaa857b4b
Matthew Marley <matthewmarley@Matthews-MacBook-Air.local>
2025-03-18 18:20:20 +0000

    Update sitemap and robots.txt with new domain: eastkilbridewebdesign.com

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://ekwebdesign.vercel.app',
+  siteUrl: 'https://www.eastkilbridewebdesign.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   outDir: 'public',
